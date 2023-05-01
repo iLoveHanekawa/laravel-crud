d6053f6efeee17e85d1871d32fb33855385746c6
Arjun Tanwar <arjunthakur900@gmail.com>
2023-05-01 10:56:58 +0530

    first form

---
+++ app/Http/Livewire/Artists/CreateForm.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Artists;
+
+use Livewire\Component;
+
+class CreateForm extends Component
+{
+    public function render()
+    {
+        return view('livewire.artists.create-form');
+    }
+}
